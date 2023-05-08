6cea7d4344029cf364ee03311936a685750147c3
Knowlng <batonelis88@gmail.com>
2023-05-09 00:16:54 +0300

    ui setup

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,73 +1,27 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
+import com.ui.gauPass.UIconstants;
+
+import java.awt.*;
+// import java.awt.event.ActionEvent;
+// import java.awt.event.ActionListener;
+ 
 public class MainFrame extends JFrame {
-    final private Font mainFont = new Font("Segoe print", Font.BOLD, 18);
-    JTextField tfPasswordLength;
-    JLabel lbOutput;
+    JFrame frame;
+    // final private Font labelFont = new Font("Gill Sans Ultra Bold", Font.ITALIC, 24);
+
 
     public void initialize() {
-        /* Settings Panel */
-        JLabel lbPasswordLength = new JLabel("Password length");
-        lbPasswordLength.setFont(mainFont);
+        setUndecorated(true);
 
-        tfPasswordLength = new JTextField();
-        tfPasswordLength.setFont(mainFont);
-
-        JPanel settingsPanel = new JPanel();
-        settingsPanel.setLayout(new GridLayout(1, 1, 5, 5));
-        settingsPanel.add(lbPasswordLength);
-        settingsPanel.add(tfPasswordLength);
-
-        /* Output Label */
-        lbOutput = new JLabel();
-        lbOutput.setFont(mainFont);
-
-        /* Button Panel */
-        JButton btnGenerate = new JButton("GENERATE");
-        btnGenerate.setFont(mainFont);
-        btnGenerate.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int passwordLength = Integer.parseInt(tfPasswordLength.getText());
-
-                PasswordGenerator passwordGenerator = (new PasswordGenerator());
-                lbOutput.setText("Your secure password is " + passwordGenerator.generate(passwordLength));
-            }
-
-        });
-
-        JButton btnClear = new JButton("Clear");
-        btnClear.setFont(mainFont);
-        btnClear.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                lbOutput.setText("");
-            }
-        });
-
-        JPanel buttonsPanel = new JPanel();
-        buttonsPanel.setLayout(new GridLayout(1, 2, 5, 5));
-        buttonsPanel.add(btnGenerate);
-        buttonsPanel.add(btnClear);
-
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BorderLayout());
-        mainPanel.setBackground(new Color(182, 73, 245));
-        mainPanel.add(settingsPanel, BorderLayout.NORTH);
-        mainPanel.add(lbOutput, BorderLayout.CENTER);
-        mainPanel.add(buttonsPanel, BorderLayout.SOUTH);
-
-        add(mainPanel);
-
-        setTitle("GauPass - Secure password generator");
-        setSize(500, 600);
+        setSize(1150, 400);
+        setLocationRelativeTo(null);
         setMinimumSize(new Dimension(300, 400));
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        // setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        
         setVisible(true);
+        
+        getContentPane().setBackground(UIconstants.FOG);
     }
 }
---
+++ src/com/ui/gauPass/UIconstants.java
@@ -0,0 +1,10 @@
+package com.ui.gauPass;
+
+import java.awt.Color;
+
+public class UIconstants {
+    public static final Color DEEP_LILAC = new Color(163, 73, 164);
+    public static final Color FOG = new Color(200, 191, 231); 
+
+}
+
