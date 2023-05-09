d38341a62eee91c84b241e24fb59f85f0e410dfb
P3tKa <petkeviciuslukas1@gmail.com>
2023-05-09 15:25:58 +0300

    Improved PasswordGenerator class

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -2,14 +2,30 @@ import java.security.SecureRandom;
 
 public class PasswordGenerator {
     private String password;
+    private boolean useNumbers;
+    private boolean useSpecialChars;
 
     PasswordGenerator() {
         password = "";
+        useNumbers = true;
+        useSpecialChars = true;
+    }
+
+    public void setUseNumbers(boolean useNumbers) {
+        this.useNumbers = useNumbers;
+    }
+
+    public void setUseSpecialChars(boolean useSpecialChars) {
+        this.useSpecialChars = useSpecialChars;
     }
 
     public String generate(int passwordLength) {
         SecureRandom random = new SecureRandom();
-        String validChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?@";
+        String validChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        if (useNumbers)
+            validChars += "0123456789";
+        if (useSpecialChars)
+            validChars += "~!@#$%^&*()_-,.?";
 
         for (int i = 0; i < passwordLength; i++) {
             int randomIndex = random.nextInt(validChars.length());
