92512f020c94b8c591a4d01cc5b1e6faaee17ceb
MasonCatto <meisonasc@gmail.com>
2023-05-16 16:27:31 +0300

    Custom password gen algorithm: interchanging symbols

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        MainFrame myFrame = new MainFrame();
-        myFrame.initialize();
+        // MainFrame myFrame = new MainFrame();
+        // myFrame.initialize();
+
+        Scanner scanner = new Scanner(System.in);
+        PasswordGenerator passwordGenerator = new PasswordGenerator();
+
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
+        System.out.println("Input a word");
+        String word = scanner.nextLine();
+        String password = passwordGenerator.generate(10,word);
+
+        System.out.println(password);
+        scanner.close();
     }
 }
--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -1,4 +1,7 @@
 import java.security.SecureRandom;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PasswordGenerator {
     private String password;
@@ -19,18 +22,63 @@ public class PasswordGenerator {
         this.useSpecialChars = useSpecialChars;
     }
 
-    public String generate(int passwordLength) {
-        SecureRandom random = new SecureRandom();
-        String validChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        if (useNumbers)
-            validChars += "0123456789";
-        if (useSpecialChars)
-            validChars += "~!@#$%^&*()_-,.?";
+    public String generate(int passwordLength, String word) {
 
-        for (int i = 0; i < passwordLength; i++) {
-            int randomIndex = random.nextInt(validChars.length());
-            password += validChars.charAt(randomIndex);
+        SecureRandom random = new SecureRandom();
+        // StringBuilder passwordBuilder = new StringBuilder();
+        if (word == null){
+            return "bruh";
+        } else {
+        StringBuilder password = new StringBuilder();
+        Map<Character, List<Character>> symbolMap = createSymbolMap();
+        
+        for (char c : word.toCharArray()) {
+            char lowerC = Character.toLowerCase(c);
+            List<Character> symbols = symbolMap.get(lowerC);
+            if (symbols != null && symbols.size() > 0) {
+                int index = random.nextInt(symbols.size());
+                password.append(symbols.get(index));
+            } else {
+                password.append(c);
+            }
         }
-        return password;
+        
+        return password.toString();
+    }
+    }
+    private static Map<Character, List<Character>> createSymbolMap() {
+        
+        Map<Character, List<Character>> symbolMap = new HashMap<>();
+
+        symbolMap.put('a', List.of('4', '@', '^', 'a', 'A'));
+        symbolMap.put('b', List.of('8', '6', '3', 'b', 'B'));
+        symbolMap.put('c', List.of('(', '<', '[', 'c', 'C'));
+        symbolMap.put('d', List.of('b', 'D', 'd', 'p', 'q','0'));
+        symbolMap.put('e', List.of('3', 'e', 'E','_'));
+        symbolMap.put('f', List.of('f', 'F'));
+        symbolMap.put('g', List.of('9', '6', 'g', 'G', 'q'));
+        symbolMap.put('h', List.of('#', 'h', 'H'));
+        symbolMap.put('i', List.of('1', '|', '!', 'i', '/', 'I'));
+        symbolMap.put('j', List.of('j', 'J'));
+        symbolMap.put('k', List.of('k', 'K'));
+        symbolMap.put('l', List.of('1', '|', 'l', 'L', 'i', '/', '!'));
+        symbolMap.put('m', List.of('m', 'M'));
+        symbolMap.put('n', List.of('n', 'N'));
+        symbolMap.put('o', List.of('0', '*', '#', 'o', 'O'));
+        symbolMap.put('p', List.of('d', 'P', 'p', 'q'));
+        symbolMap.put('q', List.of('9', '6', 'g', 'G', 'q'));
+        symbolMap.put('r', List.of('r', 'R'));
+        symbolMap.put('s', List.of('$', '5', 's', 'S'));
+        symbolMap.put('t', List.of('7', '+', 't', '7', 'T', '/'));
+        symbolMap.put('u', List.of('u', 'U'));
+        symbolMap.put('v', List.of('v', 'V','<','>'));
+        symbolMap.put('w', List.of('w', 'W'));
+        symbolMap.put('x', List.of('x', 'X'));
+        symbolMap.put('y', List.of('y', 'Y'));
+        symbolMap.put('z', List.of('2', '5', 'z', 'Z'));
+        
+        
+
+        return symbolMap;
     }
 }
